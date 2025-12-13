3e78f53c4e57c67ca47ab3a445625cfdd39b979d
Ankush Kumar <ankushkumar16217@gmail.com>
2025-12-13 20:51:26 +0530

    Fourth Commit

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@
     const PORT=process.env.PORT || 8000
 
     app.use(cors({
-      origin:'http://localhost:5173',
+      origin:'https://expenseflow-bvl5.onrender.com',
       credentials:true
     }))
     app.use(express.urlencoded({extended:false}))
